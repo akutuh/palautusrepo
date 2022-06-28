fdd26b7ab5cb4154437d7b0a1d092bdd41c8302a
akutuh <akutuh@uef.fi>
2022-06-28 09:12:21 +0300

    puhelinluettelo 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,12 +6,20 @@ const Name = ({ person }) => {
   )
 }
 
+
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilterName, setNewFilterName] = useState('')
+  const [filteredList, setFilteredList] = useState(persons)
+
 
   const addName = (event) => {
     event.preventDefault()
@@ -26,6 +34,7 @@ const App = () => {
       setPersons(persons.concat(nameObject))
       setNewName('')
       setNewNumber('')
+      setFilteredList(persons)
     }
     
   }
@@ -40,9 +49,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleNameFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilterName(event.target.value)
+    setFilteredList(persons.filter(person => person.name !== newFilterName))
+    console.log('FilteredList',filteredList)
+  }
+  /*
+    useEffect ?
+  */
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with<input value={newFilterName} onChange={handleNameFilterChange}/>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}  />
@@ -56,7 +80,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {filteredList.map(person =>
           <Name key={person.name} person={person}/>)}
       </ul>
     </div>
