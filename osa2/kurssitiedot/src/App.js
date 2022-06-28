5bb825cd7500faf476c1eb4f0f20bb36cc9284ee
akutuh <akutuh@uef.fi>
2022-06-28 06:07:37 +0300

    kurssitiedot 2.4

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -31,36 +31,63 @@ const Course = ({ course }) =>
     <Total course={course} />
   </>
 
+const Courses = ({ courses }) => {
+  return (
+    <>
+    {courses.map(course =>
+      <Course key={course.id} course={course}/>)}
+    </>
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        id: 1,
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        id: 2,
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        id: 3,
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        id: 4,
-        name: 'Redux',
-        exercises: 11
-      }
-    ]
-  }
+  const courses =[
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          id: 1,
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          id: 2,
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          id: 3,
+          name: 'State of a component',
+          exercises: 14
+        },
+        {
+          id: 4,
+          name: 'Redux',
+          exercises: 11
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   return (
     <div>
-      <Course course={course} />
+      <Courses courses={courses} />
     </div>
   )
 }
